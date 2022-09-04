f5930e4cc307fc284daeb7fb332d82fc39a9ad34
YoussefMP <youssefameur94@gmail.com>
2022-09-04 16:12:16 +0200

    removed redundant libs

--- UnloQR_Server/Website/auth.py
+++ UnloQR_Server/Website/auth.py
@@ -1,8 +1,5 @@
 import base64
 import os
-import tempfile
-from pathlib import Path
-
 from flask import Blueprint, render_template, request, flash, redirect, url_for, Response
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
